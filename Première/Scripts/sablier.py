5bf830542e44776e3002571b7d3928b15e0df6b8
Johann Dolivet <jdolivet@hotmail.com>
2020-12-05 10:58:24 -0300

    Add files via upload

---
+++ Première/Scripts/sablier.py
@@ -0,0 +1,21 @@
+from turtle import *
+
+# Rectangle épais
+width(3)
+color(0.2, 0.2, 0.2)
+goto(60, 0)
+goto(60, 110)
+goto(0, 110)
+goto(0, 0)
+# Déplacement
+up()
+goto(5, 5)
+down()
+# Sablier gris clair
+fillcolor("grey")
+begin_fill()
+goto(55 ,5)
+goto(5, 105)
+goto(55, 105)
+goto(5, 5)
+end_fill()
