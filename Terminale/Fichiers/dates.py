f23d5cbc2a84f9b4b92f889bec88b72a1cbbcf69
Johann Dolivet <jdolivet@hotmail.com>
2022-03-05 23:36:21 -0300

    Add files via upload

--- Terminale/Fichiers/dates.py
+++ Terminale/Fichiers/dates.py
@@ -2,7 +2,7 @@ def cree():
     """crée et renvoie un ensemble de dates vide"""
     return [0] * 6
 
-def contient(ens, val):
+def contient(ens, val) -> bool:
     """renvoie True si, et seulement si, l'ensemble ens contient la date val"""
     if val < 1 or val > 366:
         return False
@@ -10,7 +10,7 @@ def contient(ens, val):
     bit = val % 64
     return ens[paquet] & (1 << bit) != 0
 
-def ajoute(ens, val):
+def ajoute(ens, val) -> None:
     """ajoute la date val à l'ensemble ens"""
     if val < 1 or val > 366
         raise ValueError("date " + str(val) + " invalide")
--- Terminale/Fichiers/ensemble_v1.py
+++ Terminale/Fichiers/ensemble_v1.py
@@ -1,14 +1,18 @@
 def cree():
+    """crée et renvoie un ensemble de dates vide"""
     return [[] for _ in range(23)]
 
-def contient(ens, val):
+def contient(ens, val) -> bool:
+    """renvoie True si, et seulement si, l'ensemble ens contient la date val"""
     return val in ens[val % 23]
 
-def ajoute(ens, val):
+def ajoute(ens, val) -> None:
+    """ajoute la date val à l'ensemble ens"""
     ens[val % 23].append(val)
     
-def enumere(ens):
+def enumere(ens) -> list:
+    """Renvoie un tableau contenant les éléments de l'ensemble ens."""
     tab = []
     for paquet in ens:
         tab.extend(paquet)
-    return tabv
+    return tab
