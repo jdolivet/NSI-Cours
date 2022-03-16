93f2daa39653c2fa9a25561d0e7a5753570f99fc
Johann Dolivet <jdolivet@hotmail.com>
2022-03-16 15:53:55 -0300

    Add files via upload

--- Terminale/Fichiers/dates.py
+++ Terminale/Fichiers/dates.py
@@ -1,19 +1,34 @@
 def cree():
-    """crée et renvoie un ensemble de dates vide"""
+    """Crée et renvoie un ensemble de dates vide"""
     return [0] * 6
 
 def contient(ens, val) -> bool:
-    """renvoie True si, et seulement si, l'ensemble ens contient la date val"""
+    """Renvoie True si, et seulement si, l'ensemble ens contient la date val"""
     if val < 1 or val > 366:
         return False
     paquet = val // 64
     bit = val % 64
     return ens[paquet] & (1 << bit) != 0
 
+# Tests
+s0 = cree()
+assert contient(s0, 45) == False
+
 def ajoute(ens, val) -> None:
-    """ajoute la date val à l'ensemble ens"""
-    if val < 1 or val > 366
-        raise ValueError("date " + str(val) + " invalide")
+    """Ajoute l'élément val à l'ensemble ens si i <= x <= 366 
+    et lève une exception ValueError sinon"""
+    if val < 1 or val > 366:
+        raise ValueError(f"date {val} invalide")
     paquet = val // 64
     bit = val % 64
     ens[paquet] = ens[paquet] | (1 << bit)
+    
+# Tests
+ajoute(s0, 45)
+assert contient(s0, 45) == True
+ajoute(s0, 279)
+assert contient(s0, 279) == True
+try:
+    ajoute(s0, 389)
+except ValueError:
+    assert contient(s0, 389) == False
--- Terminale/Fichiers/ensemble_v1.py
+++ Terminale/Fichiers/ensemble_v1.py
@@ -1,14 +1,33 @@
 def cree():
-    """crée et renvoie un ensemble de dates vide"""
+    """Crée et renvoie un ensemble de dates vide"""
     return [[] for _ in range(23)]
 
 def contient(ens, val) -> bool:
-    """renvoie True si, et seulement si, l'ensemble ens contient la date val"""
+    """Renvoie True si, et seulement si, l'ensemble ens contient la date val"""
     return val in ens[val % 23]
 
+# Tests
+s0 = cree()
+assert contient(s0, 45) == False
+
+
 def ajoute(ens, val) -> None:
-    """ajoute la date val à l'ensemble ens"""
+    """Ajoute l'élément val à l'ensemble ens si i <= x <= 366 
+    et lève une exception ValueError sinon"""
+    if val < 1 or val > 366:
+        raise ValueError(f"date {val} invalide")
     ens[val % 23].append(val)
+    
+# Tests
+ajoute(s0, 45)
+assert contient(s0, 45) == True
+ajoute(s0, 279)
+assert contient(s0, 279) == True
+try:
+    ajoute(s0, 389)
+except ValueError:
+    assert contient(s0, 389) == False
+    
     
 def enumere(ens) -> list:
     """Renvoie un tableau contenant les éléments de l'ensemble ens."""
@@ -16,3 +35,20 @@ def enumere(ens) -> list:
     for paquet in ens:
         tab.extend(paquet)
     return tab
+
+# Tests
+s1 = cree()
+assert type(enumere(s1)) is list
+assert len(enumere(s1)) == 0
+
+ajoute(s1, 45)
+assert len(enumere(s1)) == 1
+assert 45 in enumere(s1)
+
+ajoute(s1, 257)
+assert len(enumere(s1)) == 2
+assert 257 in enumere(s1)
+
+ajoute(s1, 63)
+assert len(enumere(s1)) == 3
+assert 63 in enumere(s1)
